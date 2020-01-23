a2e6d9a2b6351291b658858d7dcb3e079ab2adc7
starship007 <thisiscodyr@gmail.com>
2020-01-23 17:46:56 -0600

    Added the color counting to Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,109 +7,89 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.util.Color;
 //updated version 2
 
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * This is a simple example to show how the REV Color Sensor V3 can be used to
+ * detect pre-configured colors.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;//sets up the I2C port
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);//sets up a new sensor at the port
+    private final ColorMatch m_colorMatcher = new ColorMatch();//sets up the color match system
+  //creats target colors
+   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+   private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+   private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-  private RobotContainer m_robotContainer;
+  //for the color change count vairables
+   static int numberOfChange = 0;
+   static boolean isColorTheInitial = true; //used to determine if the color has changed or not
+   static boolean controlBooleanCode = true; //used as a control when counting the number of changes
+   static boolean hasInitialColorBeenSet = false;
+   static String initialColor; //used as a placeholder for what the first color the color sensors see is
+   
+   
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+   
+
+
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);    
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
+    final Color detectedColor = m_colorSensor.getColor();
+    String colorString;
+    final ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
     }
+
+
+    if(!colorString.equals(Robot.initialColor)){
+      if(Robot.controlBooleanCode == true){
+        Robot.controlBooleanCode = false;
+        Robot.numberOfChange++;
+        System.out.println("The color was changed, and the color has changed " + Robot.numberOfChange + " times from " + Robot.initialColor );
+        
+      }        
+  } else{
+    Robot.controlBooleanCode = true;
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-  }
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
+    SmartDashboard.putString("Initial Color", Robot.initialColor);
+    SmartDashboard.putNumber("Number of changes from the inital color, " + Robot.initialColor, Robot.numberOfChange);
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
   }
 }
